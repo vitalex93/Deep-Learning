347d29b20c4e35150c1909e5a1467733466e7658
vitalex93 <vitalex93@gmail.com>
2022-06-11 12:51:40 +0300

    dataset with mel_spectrogram

--- Dataset.py
+++ Dataset.py
@@ -1,14 +1,18 @@
 import os
 
+import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
 
 class MusicSoundDataset(Dataset):
 
-    def __init__(self, annotations_file, audio_dir):
+    def __init__(self, annotations_file, audio_dir, transformation,
+                 target_sample_rate):
         self.annotations = pd.read_csv(annotations_file)
         self.audio_dir = audio_dir
+        self.transformation = transformation
+        self.target_sample_rate = target_sample_rate
 
     def __len__(self):
         return len(self.annotations)
@@ -17,7 +21,22 @@ class MusicSoundDataset(Dataset):
         audio_sample_path = self._get_audio_sample_path(index)
         label = self._get_audio_sample_label(index)
         signal, sr = torchaudio.load(audio_sample_path)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self.transformation(signal)
         return signal, label
+
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
+
+    def _mix_down_if_necessary(self, signal):
+        #singnal tensor with two dimensions (num_of_channels, samples)
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
 
     def _get_audio_sample_path(self, index):
         fold = self.annotations.iloc[index, 59]
@@ -32,7 +51,16 @@ class MusicSoundDataset(Dataset):
 if __name__ == "__main__":
     ANNOTATIONS_FILE = "/home/vitalex93/Desktop/Data_Science/Deep_Learning/DLproject/Data/features_30_sec.csv"
     AUDIO_DIR = "/home/vitalex93/Desktop/Data_Science/Deep_Learning/DLproject/Data/genres_original/"
-    md = MusicSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR)
+    SAMPLE_RATE = 16000
+
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+    md = MusicSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, mel_spectrogram,
+                            SAMPLE_RATE)
     print(f"There are {len(md)} samples in the dataset.")
     signal, label = md[0]
 

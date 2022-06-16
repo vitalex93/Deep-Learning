72e6781756cc24f04b0da16726c739ec7b9f12c7
vitalex93 <vitalex93@gmail.com>
2022-06-16 16:10:45 +0300

    spesctrogram

---
+++ Spectrogram.py
@@ -0,0 +1,44 @@
+import os
+import pickle
+
+import librosa
+import numpy as np
+
+
+
+
+class LogSpectrogramExtractor:
+    """LogSpectrogramExtractor extracts log spectrograms (in dB) from a
+    time-series signal.
+    """
+
+    def __init__(self, frame_size, hop_length, signal):
+        self.signal = signal
+        self.frame_size = frame_size
+        self.hop_length = hop_length
+
+    def extract(self):
+        stft = librosa.stft(self.signal,
+                            n_fft=self.frame_size,
+                            hop_length=self.hop_length)[:-1]
+        spectrogram = np.abs(stft)
+        log_spectrogram = librosa.amplitude_to_db(spectrogram)
+        return log_spectrogram
+
+
+class MinMaxNormaliser:
+    """MinMaxNormaliser applies min max normalisation to an array."""
+
+    def __init__(self, min_val, max_val):
+        self.min = min_val
+        self.max = max_val
+
+    def normalise(self, array):
+        norm_array = (array - array.min()) / (array.max() - array.min())
+        norm_array = norm_array * (self.max - self.min) + self.min
+        return norm_array
+
+    def denormalise(self, norm_array, original_min, original_max):
+        array = (norm_array - self.min) / (self.max - self.min)
+        array = array * (original_max - original_min) + original_min
+        return array

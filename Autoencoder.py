e076382efce7e1b16704e238cd11717626f001f6
vitalex93 <vitalex93@gmail.com>
2022-06-18 14:12:55 +0300

    dimensions

--- Autoencoder.py
+++ Autoencoder.py
@@ -27,15 +27,20 @@ class Autoencoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        #self.input = input
+        
         self.encoder = nn.Sequential( #784
+                #input volume = 28*28*1
                 nn.Conv2d(1, 32, stride=(1, 1), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
+                #output volume = [(28 + 2*1 - 3)/1] + 1 = 28  (28*28*32 = 25088)
                 nn.Conv2d(32, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
+                #output volume = [(28 + 2*1 - 3)/2] + 1 = 14 (14*14*64 = 12544)
                 nn.Conv2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
+                #output volume = [(14 + 2*1 - 3)/2] + 1 = 8 (8*8*64 = 4096)
                 nn.Conv2d(64, 64, stride=(1, 1), kernel_size=(3, 3), padding=1),
+                #output volume = [(8 + 2*1 - 3)/1] + 1 = 8 (8*8*64 = 4096)
                 nn.Flatten(),
                 nn.Linear(3136, 2)
                 )
@@ -49,9 +54,9 @@ class Autoencoder(nn.Module):
             nn.LeakyReLU(0.01),
             nn.ConvTranspose2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),                
             nn.LeakyReLU(0.01),
-            nn.ConvTranspose2d(64, 32, stride=(2, 2), kernel_size=(3, 3), padding=0),                
+            nn.ConvTranspose2d(64, 32, stride=(2, 2), kernel_size=(3, 3), padding=1),                
             nn.LeakyReLU(0.01),
-            nn.ConvTranspose2d(32, 1, stride=(1, 1), kernel_size=(3, 3), padding=0), 
+            nn.ConvTranspose2d(32, 1, stride=(1, 1), kernel_size=(3, 3), padding=1), 
             Trim(),  # 1x29x29 -> 1x28x28
             nn.Softmax(dim=1)
             )

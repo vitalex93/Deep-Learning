d3a4430954cd0ee69e2e20601e30030d886d15f4
vitalex93 <vitalex93@gmail.com>
2022-06-18 19:18:35 +0300

    dataloader

---
+++ Dataloader.py
@@ -0,0 +1,33 @@
+from Dataset import *
+from Autoencoder import Autoencoder
+from VAE import VAE
+from torch import nn
+from torch.utils.data import DataLoader
+
+BATCH_SIZE = 128
+EPOCHS = 1
+LEARNING_RATE = 0.001
+
+ANNOTATIONS_FILE = "./data/features_30_sec.csv"
+AUDIO_DIR = "./data/genres_original/"
+SAMPLE_RATE = 22050
+FRAME_SIZE = 512
+HOP_LENGTH = 256
+NUM_SAMPLES = 22050
+INPUT_SHAPE = 256
+
+
+def create_data_loader(train_data, batch_size):
+    train_dataloader = DataLoader(train_data, batch_size=batch_size)
+    return train_dataloader
+
+md = MusicSoundDataset(ANNOTATIONS_FILE, AUDIO_DIR, FRAME_SIZE, HOP_LENGTH, SAMPLE_RATE, NUM_SAMPLES)
+train_dataloader = create_data_loader(md, BATCH_SIZE)
+
+
+model = Autoencoder()
+for input, _ in train_dataloader:
+    input = input.to('cpu')
+    #print(input.size())
+        # calculate loss
+    output = model(input)

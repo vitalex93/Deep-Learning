1cf13ba46da9817c55b29f8ffade27b17b114e02
vitalex93 <vitalex93@gmail.com>
2022-06-18 14:37:12 +0300

    dim

--- Autoencoder.py
+++ Autoencoder.py
@@ -29,26 +29,34 @@ class Autoencoder(nn.Module):
 
         
         self.encoder = nn.Sequential( 
-                #input volume = 28*28*1 = 784
+                #input volume = 256*87*1 = 22272
                 nn.Conv2d(1, 32, stride=(1, 1), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
-                #output volume = [(28 + 2*1 - 3)/1] + 1 = 28  (28*28*32 = 25088)
+                #dim1 = [(256 + 2*1 - 3)/1] + 1 = 256  
+                #dim2 = [(87 + 2*1 - 3)/1] + 1 = 87   
+                # output volume = 256*87 = 22272 
                 nn.Conv2d(32, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
-                #output volume = [(28 + 2*1 - 3)/2] + 1 = 14 (14*14*64 = 12544)
+                #dim1 = [(256 + 2*1 - 3)/2] + 1 = 129  
+                #dim2 = [(87 + 2*1 - 3)/2] + 1 = 44   
+                # output volume = 129*44 = 5676 
                 nn.Conv2d(64, 64, stride=(2, 2), kernel_size=(3, 3), padding=1),
                 nn.LeakyReLU(0.01),
-                #output volume = [(14 + 2*1 - 3)/2] + 1 = 8 (8*8*64 = 4096)
+                #dim1 = [(129 + 2*1 - 3)/2] + 1 = 65  
+                #dim2 = [(44 + 2*1 - 3)/2] + 1 = 23   
+                # output volume = 65*23 = 1495 
                 nn.Conv2d(64, 64, stride=(1, 1), kernel_size=(3, 3), padding=1),
-                #output volume = [(8 + 2*1 - 3)/1] + 1 = 8 (8*8*64 = 4096)
+                #dim1 = [(65 + 2*1 - 3)/1] + 1 = 65  
+                #dim2 = [(23 + 2*1 - 3)/1] + 1 = 23   
+                # output volume = 65*23 = 1495 
                 nn.Flatten(),
-                nn.Linear(4096, 2)
+                nn.Linear(1495, 2)
                 )
 
         #self.final_linear = nn.Linear(3136, 2)
 
         self.decoder = nn.Sequential(
-            torch.nn.Linear(2, 4096),
+            torch.nn.Linear(2, 1495),
             Reshape(-1, 64, 7, 7),
             nn.ConvTranspose2d(64, 64, stride=(1, 1), kernel_size=(3, 3), padding=1),
             nn.LeakyReLU(0.01),

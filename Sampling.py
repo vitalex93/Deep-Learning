ee546ed50a98db843fe9037e717604ff245aeea3
vitalex93 <vitalex93@gmail.com>
2022-06-19 21:25:15 +0300

    sampling

--- Sampling.py
+++ Sampling.py
@@ -8,13 +8,12 @@ model = VAE()
 model.load_state_dict(torch.load(FILE))
 model.eval()
 
-for param in model.parameters():
-    print(param)
 
 
-#with torch.no_grad():
-    #new_image = model.decoder(torch.tensor([-0.0, 0.03]).to('cpu'))
-    #new_image.squeeze_(0)
-    #new_image.squeeze_(0)
-#plt.imshow(new_image.to('cpu').numpy(), cmap='binary')
-#plt.show()
+with torch.no_grad():
+    new_image = model.decoder(torch.tensor([-0.0, 0.03]).to('cpu'))
+    print(new_image.size())
+    new_image.squeeze_(0)
+    new_image.squeeze_(0)
+plt.imshow(new_image.to('cpu').numpy(), cmap='binary')
+plt.show()
